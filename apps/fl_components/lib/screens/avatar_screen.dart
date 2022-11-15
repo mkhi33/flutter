2bbf9ee57d52f7ff455e29bb5c09c07aa2ea5e58
Amilcar Antonio Rodriguez Velasquez <aarodriguezv@unah.hn>
2022-11-15 11:35:13 -0600

    Utilizando el widget CircleAvatar

---
+++ apps/fl_components/lib/screens/avatar_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class AvatarScreen extends StatelessWidget {
+  const AvatarScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Avatars Screen'),
+        actions: [
+          Container(
+            margin: const EdgeInsets.only(right: 5),
+            child: CircleAvatar(
+              backgroundColor: Colors.indigo[900],
+              child: const Text('SL'),
+            ),
+          )
+        ],
+      ),
+      body: const Center(
+        child: CircleAvatar(
+            maxRadius: 110,
+            backgroundImage: NetworkImage(
+                'https://dragonball.guru/wp-content/uploads/2021/01/goku-dragon-ball-guru.jpg')),
+      ),
+    );
+  }
+}
