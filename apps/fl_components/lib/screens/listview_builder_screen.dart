6c60fbb384930885321afd21bed372edb1fa956e
Amilcar Antonio Rodriguez Velasquez <aarodriguezv@unah.hn>
2022-11-16 11:52:49 -0600

    Creando un scroll infinito

--- apps/fl_components/lib/screens/listview_builder_screen.dart
+++ apps/fl_components/lib/screens/listview_builder_screen.dart
@@ -1,7 +1,35 @@
 import 'package:flutter/material.dart';
 
-class ListViewBuilderScreen extends StatelessWidget {
+class ListViewBuilderScreen extends StatefulWidget {
   const ListViewBuilderScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ListViewBuilderScreen> createState() => _ListViewBuilderScreenState();
+}
+
+class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
+  final List<int> imagesIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    scrollController.addListener(() {
+      print(
+          '${scrollController.position.pixels}, ${scrollController.position.maxScrollExtent}');
+      if (scrollController.position.pixels + 500 >=
+          scrollController.position.maxScrollExtent) {
+        add5();
+      }
+    });
+  }
+
+  void add5() {
+    final lastId = imagesIds.last;
+    imagesIds.addAll([1, 2, 3, 4, 5].map((e) => lastId + e));
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +39,9 @@ class ListViewBuilderScreen extends StatelessWidget {
         removeBottom: true,
         context: context,
         child: ListView.builder(
-          itemCount: 10,
+          physics: const BouncingScrollPhysics(),
+          controller: scrollController,
+          itemCount: imagesIds.length,
           itemBuilder: (BuildContext context, int index) {
             return FadeInImage(
                 width: double.infinity,
@@ -19,7 +49,7 @@ class ListViewBuilderScreen extends StatelessWidget {
                 fit: BoxFit.cover,
                 placeholder: const AssetImage('assets/jar-loading.gif'),
                 image: NetworkImage(
-                    'https://picsum.photos/500/300?image=${index + 1}'));
+                    'https://picsum.photos/500/300?image=${imagesIds[index]}'));
           },
         ),
       ),
